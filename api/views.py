162a0043bf39834ffa4240d299bebba936d071a9
kazak <kazakvova201@gmail.com>
2019-05-05 19:24:05 +0400

    v0.1.1

--- api/views.py
+++ api/views.py
@@ -2,8 +2,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import permissions, status
 
-from api.models import Test, Post
-from api.serializers import TestSerializer, QuestionSerializer, AnswerSerializer, PostSerializer
+from api.models import Test, Post, Subject
+from api.serializers import TestSerializer, QuestionSerializer, AnswerSerializer, PostSerializer, PostPostSerializer, \
+    SubjectSerializer, SubjectPostSerializer
 
 
 class TestView(APIView):
@@ -19,6 +20,34 @@ class TestView(APIView):
         }, status=status.HTTP_200_OK)
 
 
+class SubjectView(APIView):
+    permission_classes = [permissions.AllowAny, ]
+
+    def get(self, request):
+        subjects = Subject.objects.all()
+
+        serializer = SubjectSerializer(subjects)
+
+        return Response({
+            "data": serializer.data
+        }, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        subject = SubjectPostSerializer(data=request.POST)
+
+        if subject.is_valid():
+            subject.save()
+
+            return Response({
+                "message": f"Subject {request.POST.title} created"
+            }, status=status.HTTP_201_CREATED)
+
+        else:
+            return Response({
+                "message": "Error"
+            }, status=status.HTTP_404_NOT_FOUND)
+
+
 class PostView(APIView):
     permission_classes = [permissions.AllowAny, ]
 
@@ -30,3 +59,7 @@ class PostView(APIView):
         return Response({
             "data": serializer.data
         }, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        post = PostPostSerializer(data=request.data)
+
